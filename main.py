792e63814e7d21186581b54abc7c4075f699ef1c
desk-psm <psmin9325@gmail.com>
2023-05-02 14:30:11 +0900

    refactor: make space between b and c

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def bulls_cows(): # bulls and cows 로직 구현
 
 def result(res): # 결과를 출력해주는 함수
     b, c = res
-    print(f'{b}B{c}C')
+    print(f'{b}B {c}C')
 
 if __name__ == "__main__": # 난수 생성, 사용자 입력받아 위의 함수들을 사용
     # random
